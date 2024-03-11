63c73f334302a0d243e5199b9d83b8a05a76b7de
unknown <ciparodi@gmail.com>
2024-03-11 12:09:52 -0300

    fix references to model project

---
+++ Tucson.ReservationService/Tucson.Models/Entities/Reservation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tucson.Models.DTO;
+
+namespace Tucson.Models.Entities
+{
+    public class Reservation
+    {
+        public Reservation() { }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ReservationId { get; set; }
+
+        [Required]  
+        public DateTime Date { get; set; }
+
+        [Required]  
+        public int ClientId { get; set; }
+
+        [Required]
+        public string TypeOfTable { get; set; }
+
+        public Reservation(PendingReservation reservation)
+        {
+            Date = reservation.Date;
+            ClientId = reservation.ClientId;
+            TypeOfTable = reservation.TypeOfTable;
+        }
+
+        public Reservation(ReservationDTO reservationDTO)
+        {
+            Date = reservationDTO.Date;
+            ClientId = reservationDTO.ClientId;
+        }
+
+    }
+}
---
+++ Tucson.ReservationService/Tucson.Models/Enums/ETables.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tucson.Models.Enums
+{
+    public class ETable
+    {
+        public string TypeOfTable { get; set; }
+        public int Availability { get; set; }
+        public int Seats { get; set; }
+
+        public ETable() { }
+        public ETable(string typeOfTable, int amountOfTables, int amountOfSeats)
+        {
+            TypeOfTable = typeOfTable;
+            Availability = amountOfTables;
+            Seats = amountOfSeats;
+        }
+
+        public static readonly ETable SmallTable = new ETable("SmallTable",18, 2);
+        public static readonly ETable MediumTable = new ETable("MediumTable", 15, 4);
+        public static readonly ETable LargeTable = new ETable("LargeTable", 7, 6);
+
+    }
+}
